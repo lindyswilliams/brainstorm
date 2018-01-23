00be70efa5cba2d4e2e6ead392482dc3e0359977
Lindy Williams <lindywilliams@Lindys-MacBook.local>
2018-01-22 21:19:35 -0700

    changed logic- chronjob instead

--- kafka_read.py
+++ kafka_read.py
@@ -16,29 +16,31 @@ parser.add_argument('-top','--topic', help='topic to consume', required=True, ty
 parser.add_argument('-bucket','--s3_bucket', help='s3 bucket to write to', required=True, type=str)
 args = parser.parse_args()
 
+
+parser = argparse.ArgumentParser(description='Take variables for writing to s3.')
+args = parser.parse_args()
+args.top = 'weather_data'
+args.add = '172.31.17.170'
+args.mins = 5
+
+
 s3 = boto3.resource('s3')
 
 def increment():
 	statsd.increment('weather_sender.s3_write_success')
 
-while(True):
+time_start = datetime.datetime.utcnow()
+time_x_minutes = time_start + datetime.timedelta(minutes = args.mins)
 
-	time_start = datetime.datetime.utcnow()
-	time_x_minutes = time_start + datetime.timedelta(minutes = args.mins)
+consumer = KafkaConsumer(args.top, auto_offset_reset='earliest', group_id="weather-getter", bootstrap_servers=args.add)
+with open('writing_file.json', 'w') as file:
+	for msg in consumer:
+		file.write(msg.value.decode('UTF-8'))
+		file.write('\n')
+		time_val = datetime.datetime.utcnow() < time_x_minutes
+		if not time_val:
+			break
 
-
-	while(time):
-	#	try:
-		consumer = KafkaConsumer(args.top, bootstrap_servers=args.add)
-		with open('writing_file.json', 'w') as file:
-			for msg in consumer:
-				file.write(msg.value.decode('UTF-8'))
-				time = datetime.datetime.utcnow() < time_x_minutes
-				if not time:
-					break
-
-	bucket = s3.Bucket('weather-data-lindy')
-	bucket.put_object(Key="Denver/{}/{}/{}/{}/{}/weather.json".format(time_start.year, time_start.month, time_start.day, time_start.hour, time_start.minute), Body=open("writing_file.json", 'rb'))
-	increment()
-	time.sleep(300)
-
+bucket = s3.Bucket('weather-data-lindy')
+bucket.put_object(Key="weather-data/{}.json".format(int(time.time())), Body=open("writing_file.json", 'rb'))
+increment()
